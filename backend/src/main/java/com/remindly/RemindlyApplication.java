9993b1153ca6298ebe820d716c61bae78d234bd3
Sujal Choudhary <chsujal8@gmail.com>
2025-08-20 02:13:59 +0530

    Initial project setup with Spring Boot backend

---
+++ backend/src/main/java/com/remindly/RemindlyApplication.java
@@ -0,0 +1,13 @@
+package com.remindly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindlyApplication.class, args);
+	}
+
+}
