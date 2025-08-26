f460eae816abe257d3cc8526c8ad85d5fc99ba3a
Sujal Choudhary <chsujal8@gmail.com>
2025-08-27 01:44:20 +0530

    Created a user entity to connect to table

---
+++ backend/src/main/java/com/remindly/entity/User.java
@@ -0,0 +1,21 @@
+package com.remindly.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String phone;
+    private String role;
+}
